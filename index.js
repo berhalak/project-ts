eb79960f79134b8019f9eeaa42a7047dc7294902
berhalak <jaroslaw.sadzinski@gmail.com>
2019-05-07 22:39:49 +0200

    Adding recursive imports

--- index.js
+++ index.js
@@ -5,55 +5,77 @@ var nl = (process.platform === 'win32' ? '\r\n' : '\n')
 
 const readDirs = p => readdirSync(p).filter(f => statSync(join(p, f)).isDirectory())
 
-const split = text => text.replace(/\r\n/g, "\r").replace(/\n/g, "\r").split(/\r/).filter(x=> x);
+const split = text => text.replace(/\r\n/g, "\r").replace(/\n/g, "\r").split(/\r/).filter(x => x);
 
-function readAllProjects(dir, projects){
+function readAllProjects(dir, projects) {
 	// find proj file
-	const files = readdirSync(dir);
-	const proj = files.find(x=> x.endsWith(".proj"));
-	if (proj){
-		const projPath = join(dir, proj);
-		const projName = proj.replace(".proj", "");
-		const content = split(readFileSync(projPath).toString());
-		projects[projName] = {
-			path : dir,
-			refs : content
+	try {
+		const files = readdirSync(dir);
+		const proj = files.find(x => x.endsWith(".proj"));
+		if (proj) {
+			const projPath = join(dir, proj);
+			const projName = proj.replace(".proj", "");
+			const content = split(readFileSync(projPath).toString());
+			projects[projName] = {
+				path: dir,
+				refs: content
+			}
 		}
+		const dirs = readDirs(dir).filter(x => {
+			if (x.startsWith(".") || x == "node_modules") return false;
+			return true;
+		});
+		dirs.forEach(sub => {
+			const subPath = join(dir, sub);
+			readAllProjects(subPath, projects);
+		})
+	} catch (e) {
+		// ignore file system errors
+		console.warn("Ignoring directory " + dir);
 	}
-	const dirs = readDirs(dir).filter(x=> {
-		if (x.startsWith(".") || x == "node_modules") return false;
-		return true;
-	});
-	dirs.forEach(sub => {
-		const subPath = join(dir, sub);
-		readAllProjects(subPath, projects);
-	})
 }
 
-function getTsFiles(dir){
-	return readdirSync(dir).filter(x=> x.endsWith(".ts") && x != "import.ts" && x != "index.ts").map(x=> x.replace(".ts", ""));
+function getTsFiles(dir) {
+	return readdirSync(dir).filter(x => x.endsWith(".ts") && x != "import.ts" && x != "index.ts").map(x => x.replace(".ts", ""));
 }
 
-function createIndexFile(dir){
-	const subDirs = readDirs(dir).filter(x=> {
+function createIndexFile(dir) {
+	const subDirs = readDirs(dir).filter(x => {
 		if (x.startsWith(".") || x == "node_modules") return false;
 		return true;
 	})
 	const files = getTsFiles(dir);
 	const toGenerate = subDirs.concat(files);
-	writeFileSync(join(dir, "index.ts"), toGenerate.map(x=> `export * from "./${x}"`).join(nl));
+	writeFileSync(join(dir, "index.ts"), toGenerate.map(x => `export * from "./${x}"`).join(nl));
 	subDirs.forEach(sub => {
 		const fullPath = join(dir, sub);
 		createIndexFile(fullPath);
 	})
 }
 
-function createImportFile(name, path, refs, projects){	
+function createImportFile(name, path, refs, projects) {
 	let content = "";
-	// get full paths of refernces
-	const paths = refs.map(r => {
+	// build hierarchical data
+	const allRefs = refs.slice();
+	const dict = {};
+	for(const r of allRefs){
+		dict[r] = true;
 		const proj = projects[r];
-		if (!proj){
+		if (proj){
+			if (proj.refs){
+				proj.refs.forEach(x => {
+					if (dict[x])
+						return;
+					allRefs.push(x);
+					dict[x] = true;
+				});
+			}
+		}
+	}
+	// get full paths of refernces
+	const paths = allRefs.map(r => {
+		const proj = projects[r];
+		if (!proj) {
 			throw new Error(`Project ${name} references invalid project ${r}`);
 		}
 		return relative(path, proj.path).split("\\").join("/");
@@ -62,19 +84,19 @@ function createImportFile(name, path, refs, projects){
 	writeFileSync(join(path, "import.ts"), content);
 }
 
-exports.main = function(dir){
+exports.main = function (dir) {
 	// first read all projects
 	dir = dir || process.cwd();
 
 	const projects = {}
 	readAllProjects(dir, projects);
 
-	for(const name in projects){
+	for (const name in projects) {
 		refs = projects[name].refs;
 		path = projects[name].path;
 
 		createIndexFile(path);
-		if (refs && refs[0] != "export"){
+		if (refs && refs[0] != "export") {
 			createImportFile(name, path, refs, projects);
 		}
 	}
